7976d8e9baa1f611e7e072fa717e80d7d94989d4
Arthur <rauch.arthur@gmail.com>
2020-06-03 02:41:21 +0200

    project segmentation using scenes

--- scripts/multiplayer.gd
+++ scripts/multiplayer.gd
@@ -4,47 +4,57 @@ extends Control
 onready var server_tree = get_node("ColorRect/ItemList")
 var selected_item = null
 var servers = []
-
+var time = 0
+var refreshed =false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_server("test1",1, "127.0.0.1")
-	add_server("test2",2, "255.255.255.255")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	time+=delta
+	if !refreshed && time>5:
+		time=0
+		fill_list()
 
 func add_server(name:String,num:int, ip:String) -> void:
 	var text = "["+str(num)+"/20] "+name +" # " + ip
 	server_tree.add_item(text)
 	servers.append(ip)
 
-func refresh_list()->void:
+func clear_list()->void:
 	servers.clear()
 	server_tree.clear()
+
+func fill_list()->void:
+	var list = get_parent().read_list_hub()
+	if list.empty():
+		get_parent().ask_list_hub()
+	else:
+		refreshed=true
+		clear_list()
+		for server in list:
+			add_server(server[0],server[2],server[1])
 
 func _on_Button_button_up() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://menu.tscn")
 
-
 func _on_Button2_button_up() -> void:
 	var selected= server_tree.get_selected_items()
 	if selected.empty():
 		get_node("PopupDialog2").popup()
-		print("test1")
 	else:
-		print("test2")
 		get_parent().connect_to(servers[selected[0]])
 	
 func connection_fail() -> void:
 	get_node("PopupDialog").popup()
-	refresh_list()
+	fill_list()
 	
 func host_fail()->void:
 	get_node("PopupDialog3").popup()
 
 func _on_refresh_button_up() -> void:
-	refresh_list()
+	refreshed=false
+	fill_list()
--- scripts/multiplayer_root.gd
+++ scripts/multiplayer_root.gd
@@ -1,18 +1,86 @@
 extends Node
 
+# message types:
+# TO_HUB
+# 1 => server update or timeout reply
+# 2 => server closed
+# 3 => list request
+# 4 => server creation request
+# 5 => server created
+# FROM_HUB
+# 1 server list message
+# 2 timeout request
+# 3 server approval
+# 4 server disapproval
+# 5 server added
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
+const PORT_CLIENT=6744
+const PORT_HUB=6745
+const HUB_IP="127.0.0.1"
+var socketUDP = PacketPeerUDP.new()
 var gameLoad = preload("res://game.tscn")
 var game = null
+var maxTimeOut
+const maxTimeOuts = 10
+var Bname
+var hosting = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	init_udp_hub()
+	ask_list_hub()
+func _exit_tree()->void:
+	if hosting:
+		var paquet = PoolByteArray()
+		paquet.append(2)
+		paquet.append(Bname.size())
+		paquet.append_array(Bname)
+		socketUDP.put_packet(paquet)
+	socketUDP.close()
 
+func init_udp_hub()->void:
+	if (socketUDP.listen(PORT_CLIENT) != OK):
+		print("Error listening on port: " + str(PORT_CLIENT))
+	else:
+		print("Listening on port: " + str(PORT_CLIENT))
+	socketUDP.set_dest_address(HUB_IP,PORT_HUB)
 
+func ask_list_hub()->void:
+	print("asked list to hub")
+	var pac = PoolByteArray()
+	pac.append(3)
+	socketUDP.put_packet(pac)
 # creates an empty game instance
+func read_list_hub()->Array:
+	# flushes packets to only read the last one
+	var pac=PoolByteArray()
+	while socketUDP.get_available_packet_count()>0:
+		var tmp=socketUDP.get_packet()
+		print("got packet with type "+str(tmp[0])+ " from " + socketUDP.get_packet_ip() + ":" +str(socketUDP.get_packet_port()))
+		# must be a list type packet from hub
+		if tmp[0]==1 && socketUDP.get_packet_ip()==HUB_IP:
+			pac = tmp
+	if pac.empty():
+		print("no replies from hub")
+		return []
+	var list = []
+	printt("Got a reply! decoding...")
+	printt(pac.get_string_from_ascii())
+	pac=pac.subarray(1,pac.size()-1)
+	printt(pac.get_string_from_ascii())
+	while pac[0]!=70:
+		print("name size ="+str(pac[0]))
+		var name = (pac.subarray(1,pac[0])).get_string_from_ascii()
+		print(name)
+		print("ip size ="+str(pac[1+pac[0]]))
+		var ip = (pac.subarray(2+pac[0],pac[1+pac[0]]+pac[0]+1)).get_string_from_ascii()
+		print(ip)
+		var num = pac[2+pac[0]+pac[1+pac[0]]]
+		print(str(num))
+		list.append([name,ip,num])
+		pac = pac.subarray(pac[0]+pac[1+pac[0]]+3,pac.size()-1)
+	printt("decoded!")
+	return list
+
 func load_game()->void:
 	game = gameLoad.instance()
 	#remove all tiles
@@ -78,6 +146,9 @@ func set_bridge_area(pos:Vector2,rot,extents:Vector2)->void:
 
 func remove_menu()->void:
 	clean(get_node("multiplayer"))
+	clean(get_node("Timer"))
+	clean(get_node("PopupDialog"))
+	clean(get_node("Control"))
 	
 
 func clean(node:Node):
@@ -88,14 +159,56 @@ func clean(node:Node):
 
 # host button 
 func _on_Button3_button_up() -> void:
-	#create server
-	var valid = false
-	
-	if valid:
-		# don't need to remove tiles and bridges
-		game = gameLoad.instance()
+	get_node("PopupDialog").popup()
+
+
+func _on_LineEdit_text_entered(new_text: String) -> void:
+	get_node("PopupDialog").hide()
+	var name = get_node("PopupDialog/LineEdit").get_text()
+	get_node("multiplayer").hide()
+	get_node("Control").show()
+	get_node("Control/AnimatedSprite").play()
+	var paquet = PoolByteArray()
+	paquet.append(4)
+	Bname = name.to_ascii()
+	paquet.append(Bname.size())
+	paquet.append_array(Bname)
+	socketUDP.put_packet(paquet)
+	maxTimeOut=maxTimeOuts
+	get_node("Timer").start(1)
+
+
+func _on_Timer_timeout() -> void:
+	var approved = false
+	var paquet
+	while socketUDP.get_available_packet_count()>0:
+		paquet=socketUDP.get_packet()
+		print("got packet of type "+str(paquet[0]))
+		if paquet[0]==4:
+			maxTimeOut=0
+		elif paquet[0]==5:
+			approved=true
+			break
+		elif paquet[0]==3:
+			game = gameLoad.instance()
+			var pac = PoolByteArray()
+			pac.append(5)
+			pac.append(Bname.size())
+			pac.append_array(Bname)
+			pac.append(1)
+			socketUDP.put_packet(pac)
+	if !approved && maxTimeOut>0:
+		maxTimeOut=maxTimeOut-1
+		get_node("Timer").start(1)
+	elif approved:
+		hosting = true
 		self.add_child(game)
-		# notify ready to hub
 		remove_menu()
 	else:
+		if game!=null:
+			game.queue_free()
+			game=null
+		get_node("multiplayer").show()
+		get_node("Control").hide()
+		get_node("Control/AnimatedSprite").stop()
 		get_node("multiplayer").host_fail()
